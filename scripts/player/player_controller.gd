ab97f78c856c95e1af2f235bb5e8550fb3fe2a57
Reymo Je <rubicsoft0@gmail.com>
2024-12-15 10:06:28 +0700

    Delete the double jump mechanic

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -12,15 +12,17 @@ var cam_follow: bool = true
 func _process(delta) -> void:
 	camera_follows_player(cam_follow, delta)
 
+func _input(event):
+	if Global.player_controllable:
+		# Handle jump
+		jump(event)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * 5.0 * delta
 
 	if Global.player_controllable:
-		# Handle jump.
-		jump()
-
 		var direction := Input.get_axis("move_left", "move_right")
 		if direction:
 			velocity.x = direction * speed * 1000.0 * delta
@@ -33,14 +35,10 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 # Handle jump
-func jump() -> void:
-	# Double jump tracking
-	var jump_available: int = 2
-	var is_jumping: bool = false
-	
-	if Input.is_action_just_pressed("jump"):
-			velocity.y = -jump_velocity * 100.0
-			
+func jump(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_velocity * 100.0
+		print("JUMP")
 
 # Make the camera follows the player
 func camera_follows_player(is_following: bool, delta: float) -> void:
